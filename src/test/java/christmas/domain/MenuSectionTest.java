7eff8a2dfa58774773b1f7f1891628414ad9ef3c
Namyejun <yejun1148@gmail.com>
2023-11-10 11:41:02 +0900

    메뉴 섹션에 대한 테스트 작성

---
+++ src/test/java/christmas/domain/MenuSectionTest.java
@@ -0,0 +1,21 @@
+package christmas.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MenuSectionTest {
+    @Test
+    void menuSectionTest() {
+        Assertions.assertThat(MenuSection.에피타이저.toString())
+            .isEqualTo("에피타이저");
+
+        Assertions.assertThat(MenuSection.메인.toString())
+            .isEqualTo("메인");
+
+        Assertions.assertThat(MenuSection.디저트.toString())
+            .isEqualTo("디저트");
+
+        Assertions.assertThat(MenuSection.음료.toString())
+            .isEqualTo("음료");
+    }
+}
