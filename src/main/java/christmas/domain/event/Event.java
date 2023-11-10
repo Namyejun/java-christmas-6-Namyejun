5645b0f4ebcdb360737e62a22f067db58dc38b56
Namyejun <yejun1148@gmail.com>
2023-11-10 15:42:55 +0900

    사실 인터페이스를 써야 할 이유를 잘 못느끼겠어서 수정

--- src/main/java/christmas/domain/event/Event.java
+++ src/main/java/christmas/domain/event/Event.java
@@ -1,5 +1,13 @@
 package christmas.domain.event;
 
-public interface Event {
-    public int discount(int reserveDate);
+public class Event{
+    public int xmasDdayEvent(int reserveDate) {
+        int discountVal = 1000;
+
+        for (int i = 1; i < reserveDate; i++) {
+            discountVal += 100;
+        }
+
+        return discountVal;
+    }
 }
