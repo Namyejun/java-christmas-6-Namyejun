2d8f95e77a09304de55838082dd778c749d903a9
Namyejun <yejun1148@gmail.com>
2023-11-10 14:42:46 +0900

    메뉴들을 적은 항목에 오타 있어서 수정

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Menu {
     private final List<String> menus = List.of(
         "양송이수프,에피타이저,6000", "타파스,에피타이저,5500", "시저샐러드,에피타이저,8000",
-        "티본스테이크,메인,55000", "바비큐립,,메인,54000", "해산물파스타,메인,35000", "크리스마스파스타,메인,25000",
+        "티본스테이크,메인,55000", "바비큐립,메인,54000", "해산물파스타,메인,35000", "크리스마스파스타,메인,25000",
         "초코케이크,디저트,15000", "아이스크림,디저트,5000",
         "제로콜라,음료,3000", "레드와인,음료,60000", "샴페인,음료,25000"
     );
@@ -15,7 +15,7 @@ public class Menu {
 
     public Menu() {
         for (String menu : menus) {
-            String[] eachMenu = (String[]) Arrays.stream(menu.split(",")).toArray();
+            String[] eachMenu = menu.split(",");
             dishs.put(eachMenu[0], new Dish(eachMenu[0], DishSection.valueOf(eachMenu[1]), Integer.parseInt(eachMenu[2])));
         }
     }
