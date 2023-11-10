9f17ae5d8d22d1e27d640fb2013901fcb3bf6239
Namyejun <yejun1148@gmail.com>
2023-11-10 14:48:21 +0900

    양송이스프 -> 양송이수프

--- src/test/java/christmas/domain/DishTest.java
+++ src/test/java/christmas/domain/DishTest.java
@@ -8,7 +8,7 @@ public class DishTest {
     @DisplayName("수량을 입력하지 않았을 때")
     @Test
     void dishTestWithNoCount() {
-        Dish dish = new Dish("양송이스프", DishSection.에피타이저, 6000);
+        Dish dish = new Dish("양송이수프", DishSection.에피타이저, 6000);
         Assertions.assertThat(dish.getTotalPrice())
             .isEqualTo(0);
     }
@@ -16,7 +16,7 @@ public class DishTest {
     @DisplayName("수량을 입력했을 때")
     @Test
     void dishTestWithCount() {
-        Dish dish = new Dish("양송이스프", DishSection.에피타이저, 6000);
+        Dish dish = new Dish("양송이수프", DishSection.에피타이저, 6000);
         dish.setCount(3);
         Assertions.assertThat(dish.getTotalPrice())
             .isEqualTo(18000);
@@ -25,8 +25,8 @@ public class DishTest {
     @DisplayName("이름 반환 테스트")
     @Test
     void dishNameTest() {
-        Dish dish = new Dish("양송이스프", DishSection.에피타이저, 6000);
+        Dish dish = new Dish("양송이수프", DishSection.에피타이저, 6000);
         Assertions.assertThat(dish.getName())
-            .isEqualTo("양송이스프");
+            .isEqualTo("양송이수프");
     }
 }
