db550f38dfc790b1d2f0a42c8fd5de03dde7bd15
Namyejun <yejun1148@gmail.com>
2023-11-10 14:24:51 +0900

    테스트 이름 변경 및 음식 이름 반환 테스트

--- src/test/java/christmas/domain/DishTest.java
+++ src/test/java/christmas/domain/DishTest.java
@@ -15,10 +15,18 @@ public class DishTest {
 
     @DisplayName("수량을 입력했을 때")
     @Test
-    void menuTest() {
+    void menuTestWithCount() {
         Dish dish = new Dish("양송이스프", DishSection.에피타이저, 6000);
         dish.setCount(3);
         Assertions.assertThat(dish.getTotalPrice())
             .isEqualTo(18000);
+    }
+
+    @DisplayName("이름 반환 테스트")
+    @Test
+    void menuNameTest() {
+        Dish dish = new Dish("양송이스프", DishSection.에피타이저, 6000);
+        Assertions.assertThat(dish.getName())
+            .isEqualTo("양송이스프");
     }
 }
