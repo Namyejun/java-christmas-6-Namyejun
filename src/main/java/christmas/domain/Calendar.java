de93cbbdaae1ccc925fcc43ae69f6665ce9b1d3c
Namyejun <yejun1148@gmail.com>
2023-11-10 17:31:47 +0900

    weekday 조건문 수정, 주말 구하는 메소드 생성

--- src/main/java/christmas/domain/Calendar.java
+++ src/main/java/christmas/domain/Calendar.java
@@ -7,11 +7,23 @@ public class Calendar {
     private final String firstDay = "FRIDAY";
     private final List<String> days = List.of("SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY");
 
-    public List<Integer> getWeekDay() { // 금, 토를 제외한 일자를 출력
+    public List<Integer> getWeekday() { // 금, 토를 제외한 일자를 출력
         List<Integer> weekday = new ArrayList<>();
 
         for (int i = 1; i <= 31; i++) {
-            if ((i + days.indexOf(firstDay) - 1) % 7 <= 5) {
+            if ((i + days.indexOf(firstDay) - 1) % 7 < 5) {
+                weekday.add(i);
+            }
+        }
+
+        return weekday;
+    }
+
+    public List<Integer> getWeekend() { // 금, 토를 제외한 일자를 출력
+        List<Integer> weekday = new ArrayList<>();
+
+        for (int i = 1; i <= 31; i++) {
+            if ((i + days.indexOf(firstDay) - 1) % 7 >= 5) {
                 weekday.add(i);
             }
         }
