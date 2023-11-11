012dc62c5680f701d0f6c86d370d467ab425feef
Namyejun <yejun1148@gmail.com>
2023-11-11 19:31:56 +0900

    컨벤션 준수

--- src/main/java/christmas/util/InputView.java
+++ src/main/java/christmas/util/InputView.java
@@ -20,11 +20,12 @@ public class InputView {
 
     public int readDate() throws IllegalArgumentException {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+
         while (true) {
             try {
                 String input = Console.readLine();
-
                 int date = validation.validateDate(input);
+
                 return date;
             } catch (Exception e) {
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
@@ -34,10 +35,12 @@ public class InputView {
 
     public List<Dish> readOrder() throws IllegalArgumentException {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+
         while (true) {
             try {
                 String input = Console.readLine();
                 List<Dish> returnList = validation.tokenizeOrder(input, menu);
+
                 return returnList;
             } catch (Exception e) {
                 throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
