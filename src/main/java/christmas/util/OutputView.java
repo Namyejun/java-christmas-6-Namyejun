8efeb1f7ce2f85c28926238b7ca4e4ff24919e1a
Namyejun <yejun1148@gmail.com>
2023-11-11 22:16:40 +0900

    오타 수정 및 형식 변경

--- src/main/java/christmas/util/OutputView.java
+++ src/main/java/christmas/util/OutputView.java
@@ -25,16 +25,14 @@ public class OutputView {
     }
 
     public void beforeEvent() {
-        System.out.println("<할인 전 총주문 금액>");
-        System.out.println(menu.getTotalPrice());
+        System.out.println("<할인 전 총주문 금액>\n" + String.format("%,d원\n", menu.getTotalPrice()));
     }
 
     public void event() {
         System.out.println(event.serviceToString());
         System.out.println(event.toString());
         System.out.println(event.totalDiscountToString());
-        System.out.println("<할인 후 예상 거레 금액>");
-        System.out.println(String.format("%,d", menu.getTotalPrice() - event.calcTotalDiscount()));
+        System.out.println("<할인 후 예상 결제 금액>\n" + String.format("%,d원\n", menu.getTotalPrice() - event.calcTotalDiscount()));
     }
 
     public void badge() {
