4435165eac9047bfe097c77c023369e40a3816a5
Namyejun <yejun1148@gmail.com>
2023-11-11 21:45:32 +0900

    OutputView 클래스 생성

--- src/main/java/christmas/util/OutputView.java
+++ src/main/java/christmas/util/OutputView.java
@@ -1,9 +1,43 @@
 package christmas.util;
 
+import christmas.domain.Customer;
+import christmas.domain.Dish;
+import christmas.domain.Event;
+import christmas.domain.Menu;
+
 public class OutputView {
-    public void printMenu() {
-        System.out.println("<주문 메뉴>");
-        // ...
+    private Menu menu;
+    private Event event;
+    private Customer customer;
+
+    public OutputView(Menu menu, Event event, Customer customer) {
+        this.menu = menu;
+        this.event = event;
+        this.customer = customer;
     }
-    // ...
+
+    public void start() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void printMenu() {
+        System.out.println(menu.toString());
+    }
+
+    public void beforeEvent(String price) {
+        System.out.println("<할인 전 총주문 금액>");
+        System.out.println(menu.getTotalPrice());
+    }
+
+    public void event() {
+        System.out.println(event.serviceToString());
+        System.out.println(event.toString());
+        System.out.println(event.totalDiscountToString());
+        System.out.println("<할인 후 예상 겨레 금액>");
+        System.out.println(String.format("%,d", menu.getTotalPrice() - event.calcTotalDiscount()));
+    }
+
+    public void badge() {
+        System.out.println(customer.toString());
+    }
 }
